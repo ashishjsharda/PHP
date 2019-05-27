5c74fa33741e8b67d489071bbd7323fb380dac31
Ashish S <ashishjsharda@gmail.com>
2019-05-26 21:22:40 -0400

    hello world
    hello world

---
+++ helloworld.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+<title>Hello World</title>
+</head>
+
+<body>
+<?php
+        $a=2;
+        $b=6;
+        print($a+$b);
+        define("MINSIZE",50);
+        print("<br/>");
+        print(MINSIZE);
+
+        ?>
+</body>
+
+</html>
